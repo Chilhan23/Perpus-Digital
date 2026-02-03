ec995b3d20cfd5f43638ac357a00058e7a83a424
Chilhan23 <chilhansaha@gmail.com>
2026-02-04 01:26:32 +0700

    some tester

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -26,78 +26,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(), 
         ]);
 
-        // User Normal 
-        User::create([
-            'name' => 'Rayhan',
-            'username' => 'chilhan',
-            'email' => 'chilhan@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-         User::create([
-            'name' => 'Calista',
-            'username' => 'ca',
-            'email' => 'calista@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-        User::create([
-            'name' => 'Furqan',
-            'username' => 'fur',
-            'email' => 'furqan@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-        User::create([
-            'name' => 'Rivaldy',
-            'username' => 'riv',
-            'email' => 'rivaldy@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-        User::create([
-            'name' => 'Mutia',
-            'username' => 'mut',
-            'email' => 'mutia@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-        User::create([
-            'name' => 'Rizka',
-            'username' => 'riz',
-            'email' => 'rizka@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-        User::create([
-            'name' => 'Zahwa',
-            'username' => 'zah',
-            'email' => 'zahwa@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
-
-        User::create([
-            'name' => 'Najed',
-            'username' => 'naj',
-            'email' => 'najed@gmail.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => false,
-            'email_verified_at' => now(), 
-        ]);
+        
 
     }
 }
